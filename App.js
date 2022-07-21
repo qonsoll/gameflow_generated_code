4fc08698e664c583b49fad3b4b945e205eccb851
VladBogach <bogach.vladislav@gmail.com>
2022-07-21 17:14:52 +0200

    returned Orientation lock

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import 'moment/locale/nb'
 
 import { AppStackNavigator, AuthStackNavigator } from './src/navigators'
+import React, { useEffect } from 'react'
 import { UserAuthContext, UserAuthProvider, UserProvider } from './src/contexts'
 
 import { AppearanceProvider } from 'react-native-appearance'
 import { NavigationContainer } from '@react-navigation/native'
 import Orientation from 'react-native-orientation'
-import React from 'react'
 /* Importing the navigators from the navigators folder. */
 import { ThemeProvider } from '@qonsoll/react-native-design'
 import TranslationProvider from './src/contexts/TranslationContext'
 import theme from './theme'
 
-Orientation.lockToPortrait()
-
 const App = () => {
+  useEffect(() => {
+    Orientation.lockToPortrait()
+  }, [])
+
   return (
     <AppearanceProvider>
       <ThemeProvider theme={theme}>

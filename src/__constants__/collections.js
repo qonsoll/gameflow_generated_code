7d9fb582f9cd059e135ae355085783b427c2e615
VladBogach <bogach.vladislav@gmail.com>
2022-08-10 14:39:37 +0200

    #hotfix

--- src/__constants__/collections.js
+++ src/__constants__/collections.js
@@ -1,20 +1,4 @@
 const USERS = 'users'
-const VIDEO_GAMES = 'video_games'
-const TEAMS = 'teams'
-const STREAMS = 'streams'
-const COMPETITIONS = 'competitions'
-const MATCHES = 'matches'
-const GAMES = 'games'
-const GAME_MAPS = 'game_maps'
-const SCORES = 'scores'
 export const COLLECTIONS = {
-  USERS,
-  VIDEO_GAMES,
-  TEAMS,
-  STREAMS,
-  COMPETITIONS,
-  MATCHES,
-  GAMES,
-  GAME_MAPS,
-  SCORES
+  USERS
 }

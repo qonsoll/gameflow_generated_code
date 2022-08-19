2ae46e27f3f2bde0153ef484519e43a21619f7b4
olexandermushtyn <mushtynolexander@gmail.com>
2022-08-19 17:32:47 +0300

    Create form component based on react-hook-form

---
+++ src/components/Form/Form/FormItem.js
@@ -0,0 +1,83 @@
+import React, {
+  cloneElement,
+  useEffect,
+  memo,
+  useState,
+  forwardRef
+} from 'react'
+import { Animated } from 'react-native'
+import PropTypes from 'prop-types'
+import { useController, useFormContext } from 'react-hook-form'
+import { Box, Text } from '@qonsoll/react-native-design'
+import useStyles from './styles'
+
+const MARGINS = {
+  sm: 12,
+  md: 20
+}
+
+const FromItem = (props) => {
+  const { children, initialValue, label, margins = 'md', ...rest } = props
+
+  // [ADDITIONAL_HOOKS]
+  const { control, setFieldsValue } = useFormContext()
+  const {
+    field: { onChange, onBlur, value, name, ref },
+    fieldState: { error }
+  } = useController({ control, defaultValue: initialValue, ...rest })
+
+  const styles = useStyles(!!error?.message)
+
+  // [COMPONENT_STATE_HOOKS]
+  const [animatedError, setAnimatedError] = useState(error?.message)
+
+  // [COMPUTED_PROPERTIES]
+  const childrenProps = {
+    onChange,
+    value,
+    name,
+    ref,
+    error,
+    onBlur
+  }
+
+  // [USE_EFFECTS]
+  useEffect(() => {
+    initialValue && setFieldsValue({ [name]: initialValue })
+  }, [])
+
+  useEffect(() => {
+    // To have smooth out error animation set it into inner state and remove text after animation end.
+    if (animatedError && !error?.message) {
+      setTimeout(() => setAnimatedError(null), 220)
+    } else {
+      setAnimatedError(error?.message)
+    }
+  }, [error])
+
+  return (
+    <Box mb={!animatedError ? MARGINS[margins] : MARGINS[margins] - 3}>
+      {label && <Text mb={10}>{label}</Text>}
+      <FormItemChildren {...childrenProps}>{children}</FormItemChildren>
+
+      <Animated.Text style={styles.errorText}>{animatedError}</Animated.Text>
+    </Box>
+  )
+}
+
+const FormItemChildren = memo(
+  forwardRef(({ children, ...rest }, ref) => (
+    <Box mb={2}>
+      {typeof children === 'function'
+        ? children({ ...rest, ref })
+        : cloneElement(children, { ...rest, ref })}
+    </Box>
+  ))
+)
+
+FromItem.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+  label: PropTypes.string
+}
+
+export default FromItem
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,2 +1,4 @@
+export * from './Form'
+
 export { default as Input } from './Input'
 export { default as ImagePicker } from './ImagePicker'

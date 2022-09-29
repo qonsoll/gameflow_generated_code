8506b724c730c54b8fb825f2de9dae14d4f3310c
VladBogach <bogach.vladislav@gmail.com>
2022-09-29 12:56:26 +0300

    Added languages screen constant

--- src/navigators/LanguageStackNavigator.js
+++ src/navigators/LanguageStackNavigator.js
@@ -1,8 +1,9 @@
-import { LanguagesAll } from '../screens/Language'
-
 import React, { Suspense } from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+
+import { LANGUAGES_ALL_SCREEN } from '../__constants__/screens'
+import { LanguagesAll } from '../screens/Language'
 import { Spinner } from '@qonsoll/react-native-design'
+import { createStackNavigator } from '@react-navigation/stack'
 
 const Language = createStackNavigator()
 
@@ -10,11 +11,11 @@ const LanguageStackNavigator = () => {
   return (
     <Suspense fallback={<Spinner />}>
       <Language.Navigator
-        initialRouteName="LANGUAGES_ALL"
+        initialRouteName={LANGUAGES_ALL_SCREEN}
         screenOptions={{
           headerShown: false
         }}>
-        <Language.Screen name="LANGUAGES_ALL" component={LanguagesAll} />
+        <Language.Screen name={LANGUAGES_ALL_SCREEN} component={LanguagesAll} />
       </Language.Navigator>
     </Suspense>
   )
